4a7475c6ae3d0ea7bff126a1d5b4c0441cf97175
Wilder David Serna Montoya <wildser@gmail.com>
2022-03-29 17:47:02 -0500

    Add headerLogo component

--- portfolio2022/src/Components/PortfolioHeader.jsx
+++ portfolio2022/src/Components/PortfolioHeader.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 //Components
 import Navbar from './PortfolioHeaderComponents/Navbar';
+import HeaderLogo from './PortfolioHeaderComponents/HeaderLogo';
 
 //Styles
 import '../Components-CSS/PortfolioHeader.css';
@@ -9,6 +10,7 @@ import '../Components-CSS/PortfolioHeader.css';
 function PortfolioHeader() {
   return (
     <div className='portfolio--header--container'> 
+        <HeaderLogo></HeaderLogo>
         <Navbar></Navbar>
     </div>
   )
---
+++ portfolio2022/src/Components/PortfolioHeaderComponents/HeaderLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeaderLogo() {
+  return (
+    <div>
+        <img src="#" alt="Personal Logo" />
+    </div>
+  )
+}
+
+export default HeaderLogo
